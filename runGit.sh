cd7280f51a4763b30000a6a2eecde8831d938a26
Ismail Hos <ismaill.hoss@gmail.com>
2025-03-03 22:15:26 +0300

    Delete runGit.sh

--- runGit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#   Author        : Ismail Hos
-#   Date          : 04-16-24---11-10-2024
-#   Last Modified : 04-17-07---11-10-2024
-git add . 
-git commit -m "Add and Update files"
-git push origin main
